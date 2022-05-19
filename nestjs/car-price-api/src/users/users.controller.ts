7a15bc080acf9c1672aef1fc2099cce267c80e49
Elvys Cruz <ecruz@redventures.com>
2022-05-19 19:45:10 -0400

    adds car-price-api

---
+++ nestjs/car-price-api/src/users/users.controller.ts
@@ -0,0 +1,54 @@
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Patch,
+  Delete,
+  Param,
+  Query,
+  NotFoundException,
+  UseInterceptors,
+  ClassSerializerInterceptor,
+} from '@nestjs/common';
+
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { UsersService } from './users.service';
+import { SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
+
+@Controller('auth')
+export class UsersController {
+  constructor(private UsersService: UsersService) {}
+
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {
+    this.UsersService.create(body.email, body.password);
+  }
+
+  @UseInterceptors(SerializeInterceptor)
+  @Get('/:id') //incoming request is always string
+  async findUser(@Param('id') id: string) {
+    console.log('handler is running..');
+    const user = await this.UsersService.findOne(parseInt(id));
+    if (!user) {
+      throw new NotFoundException('user not found!');
+    }
+    return user;
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.UsersService.find(email);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.UsersService.remove(parseInt(id));
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.UsersService.update(parseInt(id), body);
+  }
+}
