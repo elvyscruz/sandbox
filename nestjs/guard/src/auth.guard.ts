b7da82c5d3f53603575f86fa8ec06577a4e9364e
Elvys Cruz <ecruz@redventures.com>
2022-06-17 21:51:13 -0400

    feat: add guard example

---
+++ nestjs/guard/src/auth.guard.ts
@@ -0,0 +1,17 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Request } from 'express';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request: Request = context.switchToHttp().getRequest();
+    console.log(request.query);
+    if (request.query.name !== 'peter') {
+      return false;
+    }
+    return true;
+  }
+}
